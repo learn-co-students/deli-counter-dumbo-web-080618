d9a4bcec2aaa949060f6cf42fb405b2cd1d7b8bf
Elias <eliasiscoding@gmail.com>
2019-09-20 14:04:24 -0400

    finished deli counter

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,25 +1,39 @@
-def line(k)
-  if k.length == 0
+def line(array)
+  if array.empty? # checks to see if there are any elements in the array.
     puts "The line is currently empty."
-  else
-    currentLine = "The line is currently:"
-    
-    k.each_with_index do |value, index| 
-    currentLine += " #{index.to_i+1}. #{value}"
-    end 
-    puts "#{currentLine}"
+  else 
+    current_line = "The line is currently:"
+    # counter = 1
+    # array.each do| name| 
+    #   current_line+= " #{counter}. #{name}"
+    #   counter += 1
+    # end
+    # puts "#{current_line}"
+
+    array.each.with_index(1) do |name,index|
+      current_line += " #{index}. #{name}"
+    end
+    puts "#{current_line}" 
   end 
 end 
 
-def take_a_number(katzDeliLine, name)
-  katzDeliLine.push(name)
-  puts "Welcome, #{name}. You are number #{katzDeliLine.length} in line."
-end
+def take_a_number(array,name)
+  # arr = []
+  # << - This is called the shovel method. 
+  # arr.push - This adds to the end of the array.
+  # ["juan", "joseph", "mark"]
+  array << name
+  # ["juan", "joseph", "mark","Ada"].length == 4
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end 
 
-def now_serving(k)
-  if k.length == 0 
+def now_serving(array)
+  if array.empty?
     puts "There is nobody waiting to be served!"
   else 
-    puts "Currently serving #{k.shift()}."
-  end
-end
+    # arr = ["Logan", "avi","Maria"]
+    puts "Currently serving #{array.shift}."
+  end 
+
+end 
+
